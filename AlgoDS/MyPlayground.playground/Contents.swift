29052fb679634f400f901cda593dd5423040417e
Mrigank Gupta <mrigankgupta@gmail.com>
2018-03-14 18:29:41 +0530

    Changes related to various files

--- AlgoDS/MyPlayground.playground/Contents.swift
+++ AlgoDS/MyPlayground.playground/Contents.swift
@@ -2,7 +2,8 @@
 
 import UIKit
 import DataStructSet
-
+import Foundation
+/*
 var bt = BinarySearchTree()
 var root:Node<Int>?
 //1,8,7,4,6,3,5,2
@@ -15,14 +16,19 @@ for i in [1,3,5,4,12,15,9,10] {
 //bt.height(root)
 //bt.levelOrderTraversal(root: root)
 
-//let sl = SinglyLinkedList()
-//var sroot:SNode<Int>!
-//for i in 1...10 {
-//    sroot = sl.insert(root: sroot, value: i)
-//}
-//sl.print(root: sroot)
+let sl = SinglyLinkedList<Int>()
+var sroot:SNode<Int>!
+for i in 1...10 {
+    sroot = sl.insert(root: sroot, value: i)
+}
+// Making iterator
+sl.head = sroot
+for i in sl {
+    print(i)
+}
+sl.print(root: sroot)
 
-/*
+
 var ht = HashTable<Dict<String, Int>>(count: 10)
 
 ht.update(value: Dict(k:"I", v: 1))
@@ -54,7 +60,6 @@ var inOrder = ["c","b","d","a","h","f","i","e","g"]
 var r = bt.buildBinaryTree(preOr: preOrder, inOr: inOrder)
 bt.inorderTraversal(root: r)
 bt.height(r)
-*/
 
 
 
@@ -85,40 +90,13 @@ merge(arr: &c,l:0,m:5,r:c.count-1)
 print(c)
 mergeSort(arr: &a, left: 0, right: a.count-1)
 print(a)
+ */
 var noHeap = [10,12,15,4,1,5,9,3]
 buildHeap(arr: &noHeap)
 noHeap = [7,1,2]
 heapSort(arr: &noHeap, count: noHeap.count)
 print(noHeap)
 heapify(arr: &noHeap, index: 0, count:noHeap.count)
-
-func peekFind<T>(arr:[T]) -> T? where T : Equatable & Comparable {
-    var peek: T?
-    if arr.count > 0 {
-        var i = 0
-        while i < arr.count && (peek == nil || peek! <= arr[i]) {
-            peek = arr[i]
-            i+=1
-        }
-    }
-    
-    return peek
-}
-
-func peekFindBinary<T>(arr:[T], l:Int, r:Int) -> T? where T : Equatable & Comparable {
-    
-    let index: Int = (l+r) / 2
-    if arr[index] < arr[index+1] {
-        return peekFindBinary(arr: arr, l: index+1, r: r)
-    }else if arr[index] < arr[index-1] {
-        return peekFindBinary(arr: arr, l: l, r: index-1)
-    }else {
-        print(arr[index])
-        return arr[index]
-    }
-}
-print(peekFind(arr: c))
-var i = peekFindBinary(arr: c, l: 0, r: c.count-1)
 
 
 //class NodeD<T> {
@@ -230,453 +208,108 @@ var i = peekFindBinary(arr: c, l: 0, r: c.count-1)
 //
 //prede(root: rr, value: 12)?.data
 
-//Q1
 
-
-func removeDupOSpace<T>(root:DNode<T>?) -> [T:Bool]? where T : Equatable & Comparable {
-    if root == nil {
-        return nil
+func fibnachi(_ n:Int) -> Int {
+    if n == 1 {
+        return 1
+    }else if n == 0 {
+        return 0
     }
-    var dict = [T:Bool]()
-    
-    var current = root
-    while current != nil {
-        if dict[current!.data] == nil {
-            dict[current!.data] = true
-        }
-        current = current?.next
-    }
-    return dict
+    return fibnachi(n-1) + fibnachi(n-2)
 }
 
-var rootT: DNode<Int>? = nil
-var dlist = DoubleLinkedList()
-for i in [10,1,2,1,3,4,5,4,6,7,5,3,4,1,5,8] {
-    rootT = dlist.buildLinkedList(root: rootT , data: i)
+fibnachi(4)
+
+func fibnachiIterative(_ n:Int) -> Int {
+    var fn = 0
+    var fn1 = 1
+    var fn0 = 0
+    for _ in 2...n {
+        fn = fn1 + fn0
+        fn0 = fn1
+        fn1 = fn
+    }
+    return fn
 }
+fibnachi(4)
 
-dlist.traverse(root: rootT)
-//removeDupOSpace(root: rootT)
-
-func removeDup<T>(root:DNode<T>?) -> DNode<T>? where T: Equatable & Comparable {
-    if root == nil {
-        return nil
-    }
-    var current = root
-    var look = current?.prev
-    var prev = current?.prev
-    while current != nil {
-        while look != nil {
-            if current!.data != look!.data {
-                look = look?.prev
-            }else{
-                //remove current
-                if current?.next != nil {
-                    prev?.next =  current?.next
-                    current?.next?.prev = prev
-                }else{
-                    prev?.next = nil
-                }
-                current = prev
-                break
-            }
-        }
-        look = current
-        prev = current
-        current = current?.next
-    }
-    return root
+let str = ["abc","abgl","cdf","abcd","lmn"]
+var tRoot: TrieNode<Character>?
+let trie = Trie()
+for s in str {
+    let chAr:[Character] = [Character](s) //TODO:Remind yourself
+    tRoot = trie.insert(root: tRoot, elements:chAr)
 }
-rootT = removeDup(root: rootT)
-print(dlist.traverse(root: rootT))
-
-//Q2
-
-func kthToLastElement<T>(root:DNode<T>?, kth:Int) -> T? {
-    if root == nil {
-        return nil
-    }
-    
-    var i = 0
-    var runner = root
-    while i <= kth &&  runner != nil {
-        runner = runner?.next
-        i = i + 1
-    }
-    var current = root
-    while runner != nil {
-        current = current?.next
-        runner = runner?.next
-    }
-    return current?.data
-}
-
-print(kthToLastElement(root: rootT, kth: 3))
-
-//Q3
-
-func deleteMiddle<T>(root:SNode<T>?) -> SNode<T>? {
-    if root == nil {
-        return nil
-    }
-    var runner = root
-    var current = root
-    var prev:SNode<T>?
-    var counter = 0
-    while runner != nil {
-        runner = runner?.next
-        counter = counter + 1
-        if counter % 2 == 0 {
-            prev = current
-            current = current?.next
-        }
-    }
-    //remove middle
-    prev?.next = current?.next
-    
-    return root
-}
-
-var slist = SinglyLinkedList()
-var sRoot: SNode<Int>?
-for i in [1,2,3,4,5,6,7,8,9] {
-    sRoot = slist.insert(root: sRoot, value: i)
-}
-sRoot = deleteMiddle(root: sRoot)
-print(slist.print(root: sRoot))
-
-
-//Q4
-//TODO
-func partition<T>(root:DNode<T>?, pivot:T) -> DNode<T>? where T: Equatable & Comparable {
-    if root == nil {
-        return nil
-    }
-    var length = 0
-    var current = root
-    var prev : DNode<T>?
-    while current != nil {
-        length = length + 1
-        prev = current
-        current = current?.next
-    }
-    var last = prev
-    var start = root
-    var prevToStart:DNode<T>?
-    var nextToLast:DNode<T>?
-    var i = 0
-    var j = length
-    while i < j {
-        if last!.data >= pivot {
-            nextToLast = last
-            last = last?.prev
-            j = j - 1
-        }else if start!.data < pivot {
-            prevToStart = start
-            start = start?.next
-            i = i + 1
-        }else {
-            print(start?.data)
-            print(last?.data)
-            //swap
-            let nextOfStart = start?.next
-            let prevOfLast = last?.prev
-            
-            prevToStart?.next = last
-            nextOfStart?.prev = last
-            last?.next = nextOfStart
-            last?.prev = prevToStart
-            
-            start?.next = nextToLast
-            start?.prev = prevOfLast
-            nextToLast?.prev = start
-            prevOfLast?.next = start
-            
-            start = nextOfStart
-            i = i + 1
-            last = prevOfLast
-            j = j - 1
-            print(nextOfStart?.data)
-            print(prevOfLast?.data)
-
-        }
-    }
-    return root
+for k in (tRoot?.dict.keys)! {
+    print(k)
 }
 
 
-func partitionOSpace<T>(root:SNode<T>?, pivot:T) -> SNode<T>?
-    where T: Equatable & Comparable {
-        if root == nil {
-            return nil
-        }
-        var current = root
-        var bList:SNode<T>?
-        var sList:SNode<T>?
-        var sHead:SNode<T>?
-        var bHead:SNode<T>?
+trie.search(root: tRoot, val: ["a","b"], fullType: false)
+trie.search(root: tRoot, val: ["a","b"], fullType: true)
 
-        while current != nil {
-            if current!.data < pivot {
-                if sHead == nil {
-                    sHead = current
-                    sList = current
-                }else{
-                    sList!.next = current
-                    sList = current
-                }
-            }else{
-                if bHead == nil {
-                    bHead = current
-                    bList = current
-                }else{
-                    bList!.next = current
-                    bList = current
-                }
-            }
-            current = current?.next
-        }
-        //merge
-        sList?.next = bHead
-        bList?.next = nil
-        return sHead
+trie.search(root: tRoot, val: ["a","b","d"], fullType: true)
+trie.search(root: tRoot, val: ["a","b","g"], fullType: true)
+trie.search(root: tRoot, val: ["a","b","g"], fullType: false)
+
+
+var a = [3,4,1,2,6,7,9,8,10]
+
+func heapifyMax(arr:inout [Int], index:Int) {
+
+    let left = 2*index + 1
+    let right = 2*index + 2
+    if right > arr.count-1 {
+        return
+    }
+    var max = left
+    if arr[left] < arr[right] {
+        max = right
+    }
+    if arr[max] > arr[index] {
+        arr.swapAt(max, index)
+        heapifyMax(arr: &arr, index: max)
+    }
 }
-//3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1
-slist = SinglyLinkedList()
-var sroot:SNode<Int>?
-for i in [3,5,8,5,10,2,1] {
-    sroot = slist.insert(root: sroot, value: i)
+
+func buildHeapMax(arr: inout [Int]) {
+    for i in (0...arr.count/2).reversed() {
+        heapifyMax(arr: &arr, index: i)
+    }
 }
-sroot = partitionOSpace(root: sroot, pivot: 5)
-slist.print(root: sroot)
-//Q6
-func isPalindromeOSpace<T>(root:SNode<T>?) -> Bool where T: Hashable {
-    if root == nil {
+
+buildHeapMax(arr: &a)
+
+func insertInHeap(arr:inout [Int], new:Int) {
+    arr.append(new)
+    var index = arr.count - 1
+    var parent = index/2
+    while index > 0 && arr[parent] < arr[index] {
+        arr.swapAt(parent, index)
+        index = parent
+        parent = index/2
+    }
+}
+insertInHeap(arr: &a, new: 20)
+insertInHeap(arr: &a, new: 5)
+/* TODO:
+func path(arr:[[Int]],row:Int, col:Int, i: Int, j:Int) -> Bool {
+    if arr[i][j] == 2 {
+        return true
+    }else if arr[i][j] == 0 || i < 0 || i > row || j < 0 || j > col {
         return false
     }
-    var dict = [T:Int]()
-    
-    var current = root
-    while current != nil {
-        if let count = dict[current!.data] {
-            dict[current!.data] = count + 1
-        }else{
-            dict[current!.data] = 1
-        }
-        current = current?.next
+    print("still in")
+    if path(arr: arr, row: row, col: col, i: i-1, j: j) || path(arr: arr, row: row, col: col, i: i+1, j: j)
+        || path(arr: arr, row: row, col: col, i: i, j: j-1) || path(arr: arr, row: row, col: col, i: i, j: j+1) {
+        return true
     }
-    var odd = 0
-    for (_,val) in dict {
-        if val%2 == 1 {
-            odd = odd + 1
-        }
-        if odd > 1 {
-            return false
-        }
-    }
-    return true
+    return false
 }
 
-func isPalindromeBitwise(root:SNode<Character>?) -> Bool {
-    var count = 0
-    var current = root
-    while current != nil {
-        let ascii = current!.data.ascii()
-        let masked = ascii & 31
-        print(masked)
-        let bit = 1 << masked
-        count = count ^ bit
-        current = current?.next
-    }
-    //count it
-    print(count)
-    var odd = 0
-    var bitCheck = 1
-    while bitCheck > 0 {
-        let result = count & bitCheck
-        if result > 0 {
-            odd = odd + 1
-        }
-        if odd > 1 {
-            return false
-        }
-        bitCheck = bitCheck << 1
-    }
-    return true
-}
+let mat = [[ 0 , 3 , 2 ],[ 3 , 3 , 0 ],[ 1 , 3 , 0 ]]
 
-
-var cList = SinglyLinkedList()
-var cRoot: SNode<Character>?
-var cArray:[Character] = ["a","b","c","c","a","b","d","c","c"]
-for ch in cArray {
-    cRoot = cList.insert(root: cRoot, value: ch)
-}
-isPalindromeOSpace(root: cRoot)
-isPalindromeBitwise(root: cRoot)
-
-//Q7
-
-func isIntersecting<T>(root1:SNode<T>?, root2:SNode<T>?) -> SNode<T>? {
-    if root1 == nil || root2 == nil {
-        return nil
-    }
-    //get length
-    var l1 = 0
-    var c1 = root1
-    while c1 != nil {
-        l1 = l1 + 1
-        c1 = c1?.next
-    }
-    
-    var l2 = 0
-    var c2 = root2
-    while c2 != nil {
-        l2 = l2 + 1
-        c2 = c2?.next
-    }
-    let diff = l1 - l2
-    var big = root1
-    var small = root2
-    if diff <= 0 {
-        big = root2
-        small = root1
-    }
-    var distance = abs(diff)
-    while distance > 0 {
-        big = big?.next
-        distance = distance - 1
-    }
-    while big != nil && small != nil {
-        if small !== big {
-            big = big?.next
-            small = small?.next
-        }else {
-            return small
-        }
-    }
-    return nil
-}
-var r1 = SNode(data:1)
-var r2 = SNode(data:3)
-var intersect:SNode<Int>!
-var nr1 = r1
-var nr2 = r2
-
-for i in [2,3,4,5,6,7,8,9,10] {
-    nr1.next = SNode(data:i)
-    nr1 = nr1.next!
-    if i >= 4  && i < 6 {
-        nr2.next = SNode(data:i)
-        nr2 = nr2.next!
-    }else if i == 6 {
-        nr2.next = nr1
-    }
-}
-slist.print(root: r1)
-slist.print(root: r2)
-
-print(isIntersecting(root1: r1, root2: r2)?.data)
-
-//Q8
-
-func loopDetection<T>(root:SNode<T>?) -> SNode<T>? {
-    if root == nil {
-        return nil
-    }
-    var current = root
-    var runner = root
-    while current != nil && runner != nil  {
-        current = current?.next
-        if runner?.next != nil {
-            runner = runner?.next?.next
-        }
-        if current === runner {
-            return current
-        }
-    }
-    return nil
-}
-
-var lRoot: SNode<Int> = SNode(data: 0)
-var loopNode:SNode<Int>!
-var next:SNode<Int>? = lRoot
-for i in [1,2,3,4,5,6,7,8] {
-    next?.next = SNode(data: i)
-    if i == 4 {
-        loopNode = next?.next
-    }else if i == 8 {
-        next?.next = loopNode
-    }
-    next = next?.next
-}
-
-print(loopDetection(root: lRoot)?.data)
-
-//Q5
-
-func listSum(root1:SNode<Int>?, root2:SNode<Int>?) -> SNode<Int>? {
-   
-    var c1 = root1
-    var c2 = root2
-    var s: SNode<Int>?
-    var next: SNode<Int>?
-    var carry: Int = 0
-    var sum = 0
-    var digit = 0
-    while c1 != nil && c2 != nil {
-        
-        sum = c1!.data + c2!.data + carry
-        digit = sum % 10
-        carry = sum / 10
-        if s == nil {
-            s = SNode(data: digit)
-            next = s
-        }else{
-            next?.next = SNode(data: digit)
-            next = next?.next
-        }
-        
-        c1 = c1?.next
-        c2 = c2?.next
-    }
-    while c1 == nil &&  c2 != nil {
-        sum = c2!.data + carry
-        digit = sum % 10
-        carry = sum / 10
-        next?.next = SNode(data: digit)
-        next = next?.next
-        c2 = c2?.next
-    }
-    while c2 == nil &&  c1 != nil {
-        sum = c1!.data + carry
-        digit = sum % 10
-        carry = sum / 10
-        next?.next = SNode(data: digit)
-        next = next?.next
-        c1 = c1?.next
-    }
-    return s
-}
-
-var digit1: SNode<Int>?
-for i in [7,1,6] {
-    digit1 = slist.insert(root: digit1, value: i)
-}
-
-var digit2: SNode<Int>?
-for i in [5,1] {
-    digit2 = slist.insert(root: digit2, value: i)
-}
-
-slist.print(root: listSum(root1: digit1, root2: digit2))
-
-
-
-
-
-
-
-
+path(arr: mat, row: 3, col: 3, i: 2, j: 0)
+*/
 
 
